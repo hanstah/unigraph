3c627f4b32256d9aea9d52d4193011a83e0bcc4a
hanstah <han.andrew.j@gmail.com>
2025-03-22 01:13:10 -0400

    .

--- src/components/resizerNode.tsx
+++ src/components/resizerNode.tsx
@@ -1,11 +1,5 @@
-import {
-  Handle,
-  NodeResizer,
-  Position,
-  ResizeDragEvent,
-  ResizeParams,
-} from "@xyflow/react";
-import React, { memo, useState } from "react";
+import { Handle, NodeResizer, Position } from "@xyflow/react";
+import React, { memo } from "react";
 import { NodeData } from "../core/model/Node";
 
 export type ResizerNodeDataArgs = NodeData & {
@@ -13,35 +7,35 @@ export type ResizerNodeDataArgs = NodeData & {
 };
 
 function ResizerNode({ data }: { data: ResizerNodeDataArgs }) {
-  const [dimensions, setDimensions] = useState({
-    width: data.dimensions?.width || 100,
-    height: data.dimensions?.height || 50,
-  });
+  // const [dimensions, setDimensions] = useState({
+  //   width: data.dimensions?.width || 100,
+  //   height: data.dimensions?.height || 50,
+  // });
 
   return (
     <div
-      style={{
-        width: dimensions.width,
-        height: dimensions.height,
-        position: "relative",
-      }}
+    // style={{
+    //   width: dimensions.width,
+    //   height: dimensions.height,
+    //   position: "relative",
+    // }}
     >
       <NodeResizer
         minWidth={50}
         minHeight={50}
-        onResizeEnd={(event: ResizeDragEvent, params: ResizeParams) => {
-          const newDimensions = {
-            width: params.width as number,
-            height: params.height as number,
-          };
-          setDimensions(newDimensions);
-          data.onResizeEnd?.(
-            params.x as number,
-            params.y as number,
-            newDimensions.width,
-            newDimensions.height
-          );
-        }}
+        // onResizeEnd={(event: ResizeDragEvent, params: ResizeParams) => {
+        // const newDimensions = {
+        //   width: params.width as number,
+        //   height: params.height as number,
+        // };
+        // setDimensions(newDimensions);
+        // data.onResizeEnd?.(
+        //   params.x as number,
+        //   params.y as number,
+        //   newDimensions.width,
+        //   newDimensions.height
+        // );
+        // }}
       />
       <Handle type="target" position={Position.Left} />
       <div>{data.label}</div>
